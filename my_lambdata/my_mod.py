fb38e4efd202cd6fa72d3ff29cfd82d68dc36f19
dgansen <dtgansen@gmail.com>
2020-06-04 18:55:52 -0500

    class with attributes + methods added

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import train_test_split
 def enlarge(n):
     """
     Param n is a number 
@@ -5,12 +6,28 @@ def enlarge(n):
     """
     return n*100
 
-
 def train_val_test_split(df):
-    from sklearn.model_selection import train_test_split
+    
     train, test = train_test_split(df)
     train, val = train_test_split(train)
     return(train, val, test)
+
+class coffee_bag():
+    def __init__(self, weightKG, source, variety='Natural'):
+        self.weightKG = weightKG
+        self.source = source
+        self.variety = variety
+    
+    def weight_to_lbs(self):
+        return self.weightKG * 2.204623
+
+    @staticmethod
+    def quality_reminder():
+        print('Single-origin coffee is always better!')
+
+    @property
+    def market(self):
+        return 'Commodities'
 
 
 if __name__ == "__main__":
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,14 +1,11 @@
 import pandas
-from my_mod import enlarge, train_val_test_split
+import my_mod
 
 print('Hello World')
 
-df = pandas.DataFrame({'nums': range(100)})
-print(df.head())
-
 print('---------')
-train, val, test = train_val_test_split(df)
-print('Train size',train.shape)
-print('Val size',val.shape)
-print('Test size',test.shape)
 
+cbag = my_mod.coffee_bag(50,'Bolivia')
+cbag.quality_reminder()
+
+print('Bag weight in lbs: ',cbag.weight_to_lbs())
